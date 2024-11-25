28e5114b3477221224e24679c11a697cbddce249
Sai Praneeth <praneeth.varma1028@gmail.com>
2024-11-25 19:34:14 +0530

    added prediction code

--- main.py
+++ main.py
@@ -4,20 +4,31 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from hmmlearn import hmm
+import data
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 STOCKS = {"apple": 'data/apple.csv', "dell": 'data/dell.csv', "google": 'data/google.csv'}
+
 NUM_TEST = 100
 K = 50
 NUM_ITERS=10000
 
+PLOT_SHOW = True
+
 DIR = 'performance'
+DIR_1 = 'prediction'
 
 if not os.path.exists(DIR):
     os.makedirs(DIR, exist_ok=True) 
 
+if not os.path.exists(DIR_1):
+    os.makedirs(DIR_1, exist_ok=True) 
+
 possible_states = np.arange(2, 15)
+
+def calc_mape(predicted_data, true_data):
+    return np.divide(np.sum(np.divide(np.absolute(predicted_data - true_data), true_data), 0), true_data.shape[0])
 
 for company, stock in STOCKS.items():
     dataset = np.genfromtxt(stock, delimiter = ',', skip_header = 1)
@@ -45,11 +56,10 @@ for company, stock in STOCKS.items():
 
     np.savetxt(output_file, data, delimiter=',', header='States,likelihood,aic_vect,bic_vect', comments='', fmt='%f')
 
-    PLOT_DIR = "plots"
+    PLOT_DIR_1 = "plots/aic-bic"
 
-    
-    if not os.path.exists(PLOT_DIR):
-        os.makedirs(PLOT_DIR, exist_ok=True) 
+    if not os.path.exists(PLOT_DIR_1):
+        os.makedirs(PLOT_DIR_1, exist_ok=True) 
 
     plt.figure()
     
@@ -63,9 +73,81 @@ for company, stock in STOCKS.items():
     plt.grid(True)
     plt.legend(loc='upper right')
 
-    plt.savefig(os.path.join(PLOT_DIR, f'{company}.png'))
+    plt.savefig(os.path.join(PLOT_DIR_1, f'{company}.png'))
     plt.close()
 
     opt_states = np.argmin(bic_vect) + 2
     print('Optimum number of states are {}'.format(opt_states))
 
+
+
+    for idx in reversed(range(NUM_TEST)):
+        train_data = dataset[idx + 1:,:]
+        test_data = dataset[idx,:]
+        length = train_data.shape[0]
+
+        if idx == NUM_TEST - 1:
+            model = hmm.GaussianHMM(n_components = opt_states, covariance_type = 'full', tol = 0.0001, n_iter = NUM_ITERS, init_params = 'stmc')
+        else:
+            model = hmm.GaussianHMM(n_components=opt_states, covariance_type='full', tol=0.0001, n_iter=NUM_ITERS, init_params='')
+            model.transmat_ = transmat 
+            model.startprob_ = startprob
+            model.means_ = means
+            model.covars_ = covars
+
+        model.fit(np.flipud(train_data))
+
+        transmat = model.transmat_
+        startprob = model.startprob_
+        means = model.means_
+        covars = model.covars_
+
+        if model.monitor_.iter == NUM_ITERS:
+            print('Increase number of iterations')
+            sys.exit(1)
+
+        num = 1;
+        past_likelihood = []
+        curr_likelihood = model.score(np.flipud(train_data[0:K - 1, :]))
+
+        while num < (length / K - 1):
+            past_likelihood = np.append(past_likelihood, model.score(np.flipud(train_data[num:num + K - 1, :])))
+            num = num + 1
+
+        likelihood_diff = np.argmin(np.absolute(past_likelihood - curr_likelihood))
+        predicted_change = train_data[likelihood_diff,:] - train_data[likelihood_diff + 1,:]
+        predicted_stock_data = np.vstack((predicted_stock_data, dataset[idx + 1,:] + predicted_change))
+
+    np.savetxt(f'{ DIR_1}/{company}_forecast.csv', predicted_stock_data,delimiter = ',',fmt = '%.2f')
+
+    mape = calc_mape(predicted_stock_data, np.flipud(dataset[range(100),:]))
+
+    print('MAPE for the {} stock is '.format(company),mape)
+    np.savetxt(f'results/{company}_results.csv', mape)
+
+
+    PLOT_DIR_2 = 'plots/prediction'
+
+    if not os.path.exists(PLOT_DIR_2):
+        os.makedirs(PLOT_DIR_2, exist_ok=True) 
+
+    LABELS = ['High', 'Low', 'Open', 'Close']
+
+    # Plot predicted vs actual stock prices
+    for i, label in enumerate(LABELS):
+        plt.figure()
+        plt.plot(range(100), predicted_stock_data[:, i], 'k-', label=f'Predicted {label} price')
+        plt.plot(range(100), np.flipud(dataset[range(100), i]), 'r--', label=f'Actual {label} price')
+        plt.xlabel('Time steps')
+        plt.ylabel('Price')
+        plt.title(f'{label} price for {company}')
+        plt.grid(True)
+        plt.legend(loc='upper left')
+        
+        # Save the plot
+        plt.savefig(os.path.join(PLOT_DIR_2, f'{label}_price_{company}.png'))
+        plt.close()
+        
+
+    if PLOT_SHOW:
+        plt.show(block=False)
